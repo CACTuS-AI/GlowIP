7ef6a9f86bdece11d31904a002ebf397425ab7ea
Max Daniels <qnkxsovc@gmail.com>
2020-07-08 16:44:29 -0400

    8 space indents -> 4 space indents

--- glow/coupling.py
+++ glow/coupling.py
@@ -5,107 +5,106 @@ from .net import NN
 
 # device
 if torch.cuda.is_available():
-	device = "cuda"
+    device = "cuda"
 else:
-	device = "cpu"
+    device = "cpu"
 
 
 class CouplingLayer(nn.Module):
+    def __init__(self, channels, coupling, coupling_bias, device, nn_init_last_zeros=False):
+        super(CouplingLayer, self).__init__()
+        self.coupling = coupling
+        self.channels = channels
+        self.coupling_bias = coupling_bias
+        if self.coupling == "affine":
+            self.net = NN(channels_in=self.channels // 2, channels_out=self.channels,
+                          device=device, init_last_zeros=nn_init_last_zeros)
+        elif self.coupling == "additive":
+            self.net = NN(channels_in=self.channels // 2, channels_out=self.channels // 2,
+                          device=device, init_last_zeros=nn_init_last_zeros)
+        else:
+            raise "only affine and additive coupling is implemented"
+        self.to(device)
 
-	def __init__(self, channels, coupling, coupling_bias, device, nn_init_last_zeros=False):
-		super(CouplingLayer, self).__init__()
-		self.coupling = coupling
-		self.channels = channels
-		self.coupling_bias = coupling_bias
-		if self.coupling == "affine":
-			self.net = NN(channels_in=self.channels // 2, channels_out=self.channels,
-			              device=device, init_last_zeros=nn_init_last_zeros)
-		elif self.coupling == "additive":
-			self.net = NN(channels_in=self.channels // 2, channels_out=self.channels // 2,
-			              device=device, init_last_zeros=nn_init_last_zeros)
-		else:
-			raise "only affine and additive coupling is implemented"
-		self.to(device)
+    def forward(self, x, logdet=None, reverse=False):
+        n, c, h, w = x.size()
 
-	def forward(self, x, logdet=None, reverse=False):
-		n, c, h, w = x.size()
+        if not reverse:
+            # affine coupling layer
+            if self.coupling == "affine":
+                xa, xb = self.split(x, mode="split-by-chunk")
+                s_and_t = self.net(xb)
+                s, t = self.split(s_and_t, mode="split-by-alternating")
+                s = torch.sigmoid(s + 2.) + self.coupling_bias
+                #                t       = torch.tanh(t)
+                ya = s * xa + t
+                #                ya      = torch.exp(torch.log(s+1e-6)) * xa + t
+                y = torch.cat([ya, xb], dim=1)
+                logdet = logdet + torch.log(s).view(n, -1).sum(-1)
+                assert not np.isnan(
+                    y.mean().item()), "nan in coupling forward: s=%0.4f, x=%0.4f, xa=%0.4f, ya=%0.4f, t=%0.3f" % (
+                    s.mean().item(), x.mean().item(), xa.mean().item(), ya.mean().item(), t.mean().item())
+                assert not np.isinf(
+                    y.mean().item()), "inf in coupling forward: s=%0.4f, x=%0.4f, xa=%0.4f, ya=%0.4f, t=%0.3f" % (
+                    s.mean().item(), x.mean().item(), xa.mean().item(), ya.mean().item(), t.mean().item())
+                return y, logdet
+            # additive coupling layer
+            if self.coupling == "additive":
+                xa, xb = self.split(x, mode="split-by-chunk")
+                t = self.net(xb)
+                ya = xa + t
+                y = torch.cat([ya, xb], dim=1)
+                assert not np.isnan(
+                    y.mean().item()), "nan in coupling forward: s=%0.4f, x=%0.4f, xa=%0.4f, ya=%0.4f, t=%0.3f" % (
+                    s.mean().item(), x.mean().item(), xa.mean().item(), ya.mean().item(), t.mean().item())
+                assert not np.isinf(
+                    y.mean().item()), "inf in coupling forward: s=%0.4f, x=%0.4f, xa=%0.4f, ya=%0.4f, t=%0.3f" % (
+                    s.mean().item(), x.mean().item(), xa.mean().item(), ya.mean().item(), t.mean().item())
+                return y, logdet
 
-		if not reverse:
-			# affine coupling layer
-			if self.coupling == "affine":
-				xa, xb = self.split(x, mode="split-by-chunk")
-				s_and_t = self.net(xb)
-				s, t = self.split(s_and_t, mode="split-by-alternating")
-				s = torch.sigmoid(s + 2.) + self.coupling_bias
-				#                t       = torch.tanh(t)
-				ya = s * xa + t
-				#                ya      = torch.exp(torch.log(s+1e-6)) * xa + t
-				y = torch.cat([ya, xb], dim=1)
-				logdet = logdet + torch.log(s).view(n, -1).sum(-1)
-				assert not np.isnan(
-					y.mean().item()), "nan in coupling forward: s=%0.4f, x=%0.4f, xa=%0.4f, ya=%0.4f, t=%0.3f" % (
-				s.mean().item(), x.mean().item(), xa.mean().item(), ya.mean().item(), t.mean().item())
-				assert not np.isinf(
-					y.mean().item()), "inf in coupling forward: s=%0.4f, x=%0.4f, xa=%0.4f, ya=%0.4f, t=%0.3f" % (
-				s.mean().item(), x.mean().item(), xa.mean().item(), ya.mean().item(), t.mean().item())
-				return y, logdet
-			# additive coupling layer
-			if self.coupling == "additive":
-				xa, xb = self.split(x, mode="split-by-chunk")
-				t = self.net(xb)
-				ya = xa + t
-				y = torch.cat([ya, xb], dim=1)
-				assert not np.isnan(
-					y.mean().item()), "nan in coupling forward: s=%0.4f, x=%0.4f, xa=%0.4f, ya=%0.4f, t=%0.3f" % (
-				s.mean().item(), x.mean().item(), xa.mean().item(), ya.mean().item(), t.mean().item())
-				assert not np.isinf(
-					y.mean().item()), "inf in coupling forward: s=%0.4f, x=%0.4f, xa=%0.4f, ya=%0.4f, t=%0.3f" % (
-				s.mean().item(), x.mean().item(), xa.mean().item(), ya.mean().item(), t.mean().item())
-				return y, logdet
+        if reverse:
+            # affine coupling layer
+            if self.coupling == "affine":
+                xa, xb = self.split(x, mode="split-by-chunk")
+                s_and_t = self.net(xb)
+                s, t = self.split(s_and_t, mode="split-by-alternating")
+                s = torch.sigmoid(s + 2.) + self.coupling_bias
+                #                t       = torch.tanh(t)
+                ya = (xa - t) / s
+                #                ya      = (xa  - t) * torch.exp(-torch.log(s+1e-6))
+                y = torch.cat([ya, xb], dim=1)
+                assert not np.isnan(
+                    y.mean().item()), "nan in coupling reverse: s=%0.4f, x=%0.4f, xa=%0.4f, ya=%0.4f, t=%0.3f" % (
+                    s.mean().item(), x.mean().item(), xa.mean().item(), ya.mean().item(), t.mean().item())
+                assert not np.isinf(
+                    y.mean().item()), "inf in coupling reverse: s=%0.4f, x=%0.4f, xa=%0.4f, ya=%0.4f, t=%0.3f" % (
+                    s.mean().item(), x.mean().item(), xa.mean().item(), ya.mean().item(), t.mean().item())
+                return y
+            # additive coupling layer
+            if self.coupling == "additive":
+                xa, xb = self.split(x, mode="split-by-chunk")
+                t = self.net(xb)
+                ya = (xa - t)
+                y = torch.cat([ya, xb], dim=1)
+                return y
 
-		if reverse:
-			# affine coupling layer
-			if self.coupling == "affine":
-				xa, xb = self.split(x, mode="split-by-chunk")
-				s_and_t = self.net(xb)
-				s, t = self.split(s_and_t, mode="split-by-alternating")
-				s = torch.sigmoid(s + 2.) + self.coupling_bias
-				#                t       = torch.tanh(t)
-				ya = (xa - t) / s
-				#                ya      = (xa  - t) * torch.exp(-torch.log(s+1e-6))
-				y = torch.cat([ya, xb], dim=1)
-				assert not np.isnan(
-					y.mean().item()), "nan in coupling reverse: s=%0.4f, x=%0.4f, xa=%0.4f, ya=%0.4f, t=%0.3f" % (
-				s.mean().item(), x.mean().item(), xa.mean().item(), ya.mean().item(), t.mean().item())
-				assert not np.isinf(
-					y.mean().item()), "inf in coupling reverse: s=%0.4f, x=%0.4f, xa=%0.4f, ya=%0.4f, t=%0.3f" % (
-				s.mean().item(), x.mean().item(), xa.mean().item(), ya.mean().item(), t.mean().item())
-				return y
-			# additive coupling layer
-			if self.coupling == "additive":
-				xa, xb = self.split(x, mode="split-by-chunk")
-				t = self.net(xb)
-				ya = (xa - t)
-				y = torch.cat([ya, xb], dim=1)
-				return y
-
-	def split(self, x, mode):
-		if mode == "split-by-chunk":
-			xa = x[:, :self.channels // 2, :, :]
-			xb = x[:, self.channels // 2:, :, :]
-			return xa, xb
-		if mode == "split-by-alternating":
-			xa = x[:, 0::2, :, :].contiguous()
-			xb = x[:, 1::2, :, :].contiguous()
-			return xa, xb
+    def split(self, x, mode):
+        if mode == "split-by-chunk":
+            xa = x[:, :self.channels // 2, :, :]
+            xb = x[:, self.channels // 2:, :, :]
+            return xa, xb
+        if mode == "split-by-alternating":
+            xa = x[:, 0::2, :, :].contiguous()
+            xb = x[:, 1::2, :, :].contiguous()
+            return xa, xb
 
 
 if __name__ == "__main__":
-	size = (16, 64, 32, 32)
-	coupling = CouplingLayer(channels=64, coupling="affine", device=device, nn_init_last_zeros=False)
-	x = torch.tensor(np.random.normal(5, 10, size), dtype=torch.float, device=device)
-	logdet = torch.tensor(0, dtype=torch.float, device=device, requires_grad=True)
-	y, logdet = coupling(x, logdet=logdet, reverse=False)
-	x_rev = coupling(y, reverse=True)
-	loss_rev = torch.norm(x_rev - x)
-	print(loss_rev.item())
+    size = (16, 64, 32, 32)
+    coupling = CouplingLayer(channels=64, coupling="affine", device=device, nn_init_last_zeros=False)
+    x = torch.tensor(np.random.normal(5, 10, size), dtype=torch.float, device=device)
+    logdet = torch.tensor(0, dtype=torch.float, device=device, requires_grad=True)
+    y, logdet = coupling(x, logdet=logdet, reverse=False)
+    x_rev = coupling(y, reverse=True)
+    loss_rev = torch.norm(x_rev - x)
+    print(loss_rev.item())
--- glow/flow.py
+++ glow/flow.py
@@ -7,73 +7,73 @@ from .coupling import CouplingLayer
 
 # device
 if torch.cuda.is_available():
-	device = "cuda"
+    device = "cuda"
 else:
-	device = "cpu"
+    device = "cpu"
 
 
 class Flow(nn.Module):
 
-	def __init__(self, channels, coupling, device, coupling_bias, nn_init_last_zeros=False):
-		super(Flow, self).__init__()
-		self.actnorm = ActNorm(channels, device)
-		self.coupling = CouplingLayer(channels, coupling, coupling_bias, device, nn_init_last_zeros)
-		self.invconv = InvertibleConvolution(channels, device)
-		self.to(device)
+    def __init__(self, channels, coupling, device, coupling_bias, nn_init_last_zeros=False):
+        super(Flow, self).__init__()
+        self.actnorm = ActNorm(channels, device)
+        self.coupling = CouplingLayer(channels, coupling, coupling_bias, device, nn_init_last_zeros)
+        self.invconv = InvertibleConvolution(channels, device)
+        self.to(device)
 
-	def forward(self, x, logdet=None, reverse=False):
-		if not reverse:
-			x, logdet, actnormloss = self.actnorm(x, logdet=logdet, reverse=reverse)
-			assert not np.isnan(x.mean().item()), "nan after actnorm in forward"
-			assert not np.isinf(x.mean().item()), "inf after actnorm in forward"
-			assert not np.isnan(logdet.sum().item()), "nan in log after actnorm in forward"
-			assert not np.isinf(logdet.sum().item()), "inf in log after actnorm in forward"
+    def forward(self, x, logdet=None, reverse=False):
+        if not reverse:
+            x, logdet, actnormloss = self.actnorm(x, logdet=logdet, reverse=reverse)
+            assert not np.isnan(x.mean().item()), "nan after actnorm in forward"
+            assert not np.isinf(x.mean().item()), "inf after actnorm in forward"
+            assert not np.isnan(logdet.sum().item()), "nan in log after actnorm in forward"
+            assert not np.isinf(logdet.sum().item()), "inf in log after actnorm in forward"
 
-			x, logdet = self.invconv(x, logdet=logdet, reverse=reverse)
-			assert not np.isnan(x.mean().item()), "nan after invconv in forward"
-			assert not np.isinf(x.mean().item()), "inf after invconv in forward"
-			assert not np.isnan(logdet.sum().item()), "nan in log after invconv"
-			assert not np.isinf(logdet.sum().item()), "inf in log after invconv"
+            x, logdet = self.invconv(x, logdet=logdet, reverse=reverse)
+            assert not np.isnan(x.mean().item()), "nan after invconv in forward"
+            assert not np.isinf(x.mean().item()), "inf after invconv in forward"
+            assert not np.isnan(logdet.sum().item()), "nan in log after invconv"
+            assert not np.isinf(logdet.sum().item()), "inf in log after invconv"
 
-			x, logdet = self.coupling(x, logdet=logdet, reverse=reverse)
-			assert not np.isnan(x.mean().item()), "nan after coupling in forward"
-			assert not np.isinf(x.mean().item()), "inf after coupling in forward"
-			assert not np.isnan(logdet.sum().item()), "nan in log after coupling"
-			assert not np.isinf(logdet.sum().item()), "inf in log after coupling"
+            x, logdet = self.coupling(x, logdet=logdet, reverse=reverse)
+            assert not np.isnan(x.mean().item()), "nan after coupling in forward"
+            assert not np.isinf(x.mean().item()), "inf after coupling in forward"
+            assert not np.isnan(logdet.sum().item()), "nan in log after coupling"
+            assert not np.isinf(logdet.sum().item()), "inf in log after coupling"
 
-			return x, logdet, actnormloss
+            return x, logdet, actnormloss
 
-		if reverse:
-			x = self.coupling(x, reverse=reverse)
-			assert not np.isnan(x.mean().item()), "nan after coupling in reverse"
-			assert not np.isinf(x.mean().item()), "inf after coupling in reverse"
+        if reverse:
+            x = self.coupling(x, reverse=reverse)
+            assert not np.isnan(x.mean().item()), "nan after coupling in reverse"
+            assert not np.isinf(x.mean().item()), "inf after coupling in reverse"
 
-			x = self.invconv(x, reverse=reverse)
-			assert not np.isnan(x.mean().item()), "nan after invconv in reverse"
-			assert not np.isinf(x.mean().item()), "inf after invconv in reverse"
+            x = self.invconv(x, reverse=reverse)
+            assert not np.isnan(x.mean().item()), "nan after invconv in reverse"
+            assert not np.isinf(x.mean().item()), "inf after invconv in reverse"
 
-			x = self.actnorm(x, reverse=reverse)
-			assert not np.isnan(x.mean().item()), "nan after actnorm in reverse"
-			assert not np.isinf(x.mean().item()), "inf after actnorm in reverse"
-			return x
+            x = self.actnorm(x, reverse=reverse)
+            assert not np.isnan(x.mean().item()), "nan after actnorm in reverse"
+            assert not np.isinf(x.mean().item()), "inf after actnorm in reverse"
+            return x
 
 
 if __name__ == "__main__":
-	size = (16, 4, 32, 32)
-	flow = Flow(channels=4, coupling="affine", device=device, nn_init_last_zeros=False)
-	opt = torch.optim.Adam(flow.parameters(), lr=0.01)
-	for i in range(5000):
-		opt.zero_grad()
-		x = torch.tensor(np.random.normal(1, 1, size), dtype=torch.float, device=device)
-		logdet = torch.tensor(0, dtype=torch.float, device=device, requires_grad=True)
-		y_true = x * 2 + 1
-		y, logdet, actloss = flow(x, logdet=logdet, reverse=False)
-		x_rev = flow(y, reverse=True)
-		mse = torch.norm(y_true - y)
-		mse.backward()
-		opt.step()
-		mu = y.mean().item()
-		std = y.std().item()
-		loss_rev = torch.norm(x_rev - x).item()
-		print("\rmse=%0.3f | mu=%0.3f | std=%0.3f | rloss=%0.3f | logdet=%0.3f"
-		      % (mse.item(), mu, std, loss_rev, logdet.mean().item()), end="\r")
+    size = (16, 4, 32, 32)
+    flow = Flow(channels=4, coupling="affine", device=device, nn_init_last_zeros=False)
+    opt = torch.optim.Adam(flow.parameters(), lr=0.01)
+    for i in range(5000):
+        opt.zero_grad()
+        x = torch.tensor(np.random.normal(1, 1, size), dtype=torch.float, device=device)
+        logdet = torch.tensor(0, dtype=torch.float, device=device, requires_grad=True)
+        y_true = x * 2 + 1
+        y, logdet, actloss = flow(x, logdet=logdet, reverse=False)
+        x_rev = flow(y, reverse=True)
+        mse = torch.norm(y_true - y)
+        mse.backward()
+        opt.step()
+        mu = y.mean().item()
+        std = y.std().item()
+        loss_rev = torch.norm(x_rev - x).item()
+        print("\rmse=%0.3f | mu=%0.3f | std=%0.3f | rloss=%0.3f | logdet=%0.3f"
+              % (mse.item(), mu, std, loss_rev, logdet.mean().item()), end="\r")
